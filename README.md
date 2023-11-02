86b793578e792c2f2163dfe049bfff18d258fa11
Sufian Adnan <77904242+sufianadnan@users.noreply.github.com>
2023-11-02 17:04:37 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# Random Password Generator
+
+## Overview
+
+This is a simple web application that generates random passwords. It provides a user-friendly interface to create secure passwords and allows you to copy them to your clipboard for easy use.
+
+## Features
+
+- Generate random passwords with various character types.
+- Copy generated passwords to the clipboard.
+- Stylish and responsive user interface.
+
+## Usage
+
+1. Open the `index.html` file in your web browser.
+2. Click the "Generate Password" button to create a random password.
+3. The generated password will appear in the input field.
+4. Click the "Copy" icon to copy the password to your clipboard.
+
+## Technologies Used
+
+- HTML
+- CSS
+- JavaScript
+
+## Folder Structure
+
+- `index.html`: The main HTML file for the web application.
+- `style.css`: The CSS file for styling the application.
+- `script.js`: The JavaScript file containing the logic for password generation and copying.
+- `images/`: A directory containing image assets used in the application.
---
+++ script.js
@@ -0,0 +1,38 @@
+const passwordBox = document.getElementById("password");
+const length = 12;
+
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const numbers = "0123456789";
+const symbols = "!@#$%^&*()-_+=[]{}|;:',.<>/?";
+const allChars = upperCase + lowerCase + numbers + symbols;
+function createPassword(){
+    let password = "";
+    password += upperCase[Math.floor(Math.random() * upperCase.length)];
+    password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+    password += numbers[Math.floor(Math.random() * numbers.length)];
+    password += symbols[Math.floor(Math.random() * symbols.length)];
+
+    while (length > password.length){
+        password += allChars[Math.floor(Math.random() * allChars.length)];
+
+    }
+    passwordBox.value = password;
+
+}
+
+
+
+function copyPassword(){
+    if (passwordBox) {
+        passwordBox.select();
+        document.execCommand("copy");
+
+        // You can also use the Clipboard API for modern browsers
+        navigator.clipboard.writeText(passwordBox.value).then(function() {
+            console.log('Password copied to clipboard');
+        }).catch(function(err) {
+            console.error('Failed to copy password: ', err);
+        });
+    }
+}
